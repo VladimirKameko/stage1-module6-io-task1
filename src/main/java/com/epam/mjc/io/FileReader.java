5d0ec938b210aafbe0281c5631c431edfde06da4
VladimirKameko <drugs1360@gmail.com>
2023-05-16 15:36:21 +0300

    first commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,47 @@
 package com.epam.mjc.io;
 
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
-    }
+	public Profile getDataFromFile(File file) {
+
+		try (InputStream inputStream = new FileInputStream(file)) {
+
+			StringBuilder sb = new StringBuilder();
+			int i;
+			List<String> ls = new ArrayList<>();
+
+
+			while ((i = inputStream.read()) != -1) {
+				sb.append((char) i);
+			}
+			String res = sb.toString();
+			res = res.trim();
+			String[] arr = res.split("\\n");
+			int counter = 0;
+			for (String str : arr) {
+				ls.add(str.substring(str.indexOf(":") + 1).trim());
+				System.out.println(ls);
+			}
+
+			return new Profile(ls.get(0), Integer.parseInt(ls.get(1)), ls.get(2), Long.parseLong(ls.get(3)));
+
+
+		} catch (
+				IOException e) {
+			System.err.println(e.getMessage());
+		}
+		return new Profile();
+
+	}
 }
